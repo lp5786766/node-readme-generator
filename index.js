1912d90d1cc5bad1a5d61cf4ea67e78c82caaf36
Luba Pecheneva <lp5786766@gmail.com>
2020-05-25 21:06:11 -0400

    Added initial files

---
+++ index.js
@@ -0,0 +1,28 @@
+//The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+
+const questions = [
+    // this is going to be an Array of questions:
+    
+// * What is your Project title? [string]
+// * What is your Description? [string]
+// * What is your Table of Contents? [Array: string]
+// * How to install? [string]
+// * What is your Usage [string]
+// * What is your license? [string]
+// * Contributing [Array: string]
+// * What are your tests? [string] (markdown formatted?)
+// * What are your questions? [Array: string]
+    // * What is your user GitHub profile picture? [string] (image url?)
+    // * What is your user GitHub email? [string]
+];
+
+function writeToFile(fileName, data) {
+    //where do I want the file to be placed?desktop? local directory? do I need to check?
+    //
+}
+
+function init() {
+
+}
+
+init();
