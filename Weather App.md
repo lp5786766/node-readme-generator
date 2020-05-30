3c4701bcc96a44f525f8cb1266e0a5021a78abe4
Luba Pecheneva <lp5786766@gmail.com>
2020-05-30 18:20:00 -0400

    added exaple of md file

---
+++ Weather App.md
@@ -0,0 +1,31 @@
+# "Title" ///////////
+
+badge
+
+## Description
+/////////
+
+## Table of Contents
+
+- [Delete tasks](###delete-tasks) ///////////
+
+
+## Installation
+///////////
+
+## Usage
+/////////
+
+## License
+/////////////
+
+## Contributing
+///////////
+
+## Test
+///////////
+
+## Questions:
+- GitHub profile
+- Email
+
--- index.js
+++ index.js
@@ -1,28 +1,89 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+
 //The user will be prompted for their GitHub username and other information pertaining to the project the README is for.
+inquirer.prompt([
+    {
+        type: "input",
+        name: "Title",
+        message: "What is your Project title?",
+    },
+    {
+        type: "input",
+        name: "Description",
+        message: "What is your Description?",
+    },
+    {
+        type: "input",
+        name: "Table of Contents",
+        message: "What is your table of contents?",
+    },
+    {
+        type: "input",
+        name: "Installation",
+        message: "How to install the dependencies for your product?",
+    },
+    {
+        type: "input",
+        name: "Usage",
+        message: "What is the usage of this product?", 
+    },
+    {
+        type: "input",
+        name: "License",
+        message: "What is the license for this product?",
+    },    
+    {
+        type: "input",
+        name: "Contributing",
+        message: "Who are the contibuting authors for this product?",
+    },
+    {
+        type: "input",
+        name: "Tests",
+        message: "How to run the tests for this product?",
+    },
+    {
+        type: "input",
+        name: "Your GitHub profile",
+        message: "What is your GitHub profile?",
+    },
+    {
+        type: "input",
+        name: "Your Email",
+        message: "What is your email?",
+    },
 
-const questions = [
-    // this is going to be an Array of questions:
+]).then((answers) => {
     
-// * What is your Project title? [string]
-// * What is your Description? [string]
-// * What is your Table of Contents? [Array: string]
-// * How to install? [string]
-// * What is your Usage [string]
-// * What is your license? [string]
-// * Contributing [Array: string]
-// * What are your tests? [string] (markdown formatted?)
-// * What are your questions? [Array: string]
-    // * What is your user GitHub profile picture? [string] (image url?)
-    // * What is your user GitHub email? [string]
-];
+    function getTemplateMd(answers) {
+        return ``
+    }
 
-function writeToFile(fileName, data) {
-    //where do I want the file to be placed?desktop? local directory? do I need to check?
-    //
+
+
+
+
+
+    // writeToFile(argv, answers);
+});
+
+
+function writeToFile(fileName, answers) {
+    // where do I want the file to be placed?desktop? local directory? do I need to check?
+    fs.writeFile(fileName, answers, (err) => {
+        if (err) {
+            console.error(err);
+        }
+        console.log("File saved successfully!");
+    });   
 }
 
-function init() {
+// function init() {
 
-}
+// }
 
-init();
+
+
+
+// init();
